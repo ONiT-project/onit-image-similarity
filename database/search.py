fc648fd2a9c5bae0d8d45320f822887a4c833fc5
Rainer Simon <aboutgeo@gmail.com>
2023-03-14 08:38:27 +0100

    Cleanup + full nearest neighbour search

--- database/search.py
+++ database/search.py
@@ -1,23 +1,45 @@
 import json
 import requests
 
-QDRANT_URL = 'http://localhost:6333/collections/onit/points/scroll'
+QDRANT_URL = 'http://localhost:6333/collections/onit/points'
+
+K = 20
 
 headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
-query = {
-  'filter': {
-    'must': [
-      { 'key': 'title', 'match': { 'value': 'Z15957400X_00125_page125_01' } }
-    ]
-  },
-  'with_vector': True
-}
+def search_nearest_neighbours(id, k=K):
+  query = {
+    'filter': {
+      'must': [
+        { 'key': 'title', 'match': { 'value': id } }
+      ]
+    },
+    'with_vector': True
+  }
 
-response = requests.post(QDRANT_URL, data=json.dumps(query), headers=headers)
+  response = requests.post(QDRANT_URL + '/scroll', data=json.dumps(query), headers=headers)
 
-if response.status_code != 200:
-  print(f"Error searching for point: {response.text}")
-else:
-  result = response.json()
-  print(result['result'])
+  if response.status_code != 200:
+    print(f'Record not found: {id}')
+  else:
+    top_result = response.json()['result']['points'][0]
+
+    response = requests.post(QDRANT_URL + '/search', headers=headers, data=json.dumps({
+      'vector': top_result['vector'],
+      'limit': k + 1, # Response *may* include the item itself, which we'll filter
+      'with_payload': True
+    }))
+
+    if response.status_code != 200:
+      print(f'Error: {response.text}')
+    else:
+      return response.json()['result']
+    
+
+"""
+Run sample search
+"""
+
+neighbours = search_nearest_neighbours('Z15957400X_00125_page125_01')
+print(neighbours)
+
